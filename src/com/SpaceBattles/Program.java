70e0be0424609fb5b6936069462e173f296f3f22
Ryan Kaszubski <47284437+ryanksz@users.noreply.github.com>
2021-01-21 16:41:39 -0500

    Initial commit

---
+++ src/com/SpaceBattles/Program.java
@@ -0,0 +1,22 @@
+package com.SpaceBattles;
+/**
+ * SpaceBattles!!
+ * A 2d space shooter
+ * By: Ryan Kaszubski
+ * This game uses the LITIENGINE
+ * https://litiengine.com/
+ *
+ */
+
+
+
+import de.gurkenlabs.litiengine.Game;
+
+public class Program {
+    public static void main(String[] args) {
+        //What we need to display the game windo
+        //https://litiengine.com/docs/getting-started/run-the-game/
+        Game.init(args);
+        Game.start();
+    }
+}
