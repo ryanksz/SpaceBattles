f8df4cfe9b4532f54f36f8c1b293db2b2b8d7318
Ryan Kaszubski <gn2289@wayne.edu>
2021-01-31 16:41:37 -0500

    added player sprite, changed map

--- maps/blue_stars.tsx
+++ maps/blue_stars.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="bluestars" tilewidth="8" tileheight="4" tilecount="2048" columns="32">
+ <grid orientation="orthogonal" width="16" height="16"/>
  <image source="blue.png" width="256" height="256"/>
 </tileset>
